9c3335ee92695ac2a4378f4dda2d36dc102402af
mergehez <argesozdogan@gmail.com>
2024-09-15 12:59:41 +0200

    added ILink component

---
+++ lib/Components/ILink.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {Link as InertiaLink} from '@inertiajs/vue3'
+import {InertiaLinkProps} from "@inertiajs/vue3";
+
+defineProps<InertiaLinkProps>()
+</script>
+
+<template>
+    <InertiaLink v-bind="$props">
+        <slot></slot>
+    </InertiaLink>
+</template>
